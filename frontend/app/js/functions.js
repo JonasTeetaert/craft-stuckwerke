c59453ba5f4423d1c82f4c686678cb57f4c7fa40
Jonas Teetaert <jteetaert95@gmail.com>
2020-01-29 21:19:20 +0100

    styling fixes + cross browser + strcuture fixes

---
+++ frontend/app/js/functions.js
@@ -0,0 +1,52 @@
+export default function fadeIn(targets) {
+  const options = {
+    threshold: 1.0,
+  }
+  if ('IntersectionObserver' in window) {
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const target = entry.target;
+          target.classList.add("show");
+          intersectionObserver.unobserve(target);
+        }
+      });
+    }, options);
+
+    targets.forEach(function (target) {
+      intersectionObserver.observe(target);
+    });
+  } else {
+    // Possibly fall back to a more compatible method here
+    console.log('IntersectionObserver in not supported for this browser');
+  }
+};
+
+// Source: http://fofwebdesign.co.uk/template/_testing/scroll-in-view/add-class-to-element-when-in-view.htm
+export default function elementFromBottom(elem, classToAdd, distanceFromBottom) {
+  elem.forEach(el => {
+    const distTop = el.getBoundingClientRect().top;
+    const distBottom = window.innerHeight - distTop;
+    if (distBottom >= distanceFromBottom) {
+      el.classList.add(classToAdd);
+    } else {
+      el.classList.remove(classToAdd);
+    }
+  });
+};
+
+// Ryan Taylor comment - https://remysharp.com/2010/07/21/throttling-function-calls
+export default function throttle(fn, ms) {
+  var time, last = 0;
+  return function () {
+    var a = arguments,
+      t = this,
+      now = +(new Date),
+      exe = function () {
+        last = now;
+        fn.apply(t, a);
+      };
+    clearTimeout(time);
+    (now >= last + ms) ? exe(): time = setTimeout(exe, ms);
+  }
+};
---
+++ frontend/app/js/main.js
@@ -0,0 +1,78 @@
+import Swiper from 'swiper';
+import {
+	fadeIn,
+	throttle,
+	elementFromBottom
+} from './functions';
+
+document.addEventListener('DOMContentLoaded', () => {
+	console.log('hello earth people');
+
+	// scroll to target on load
+	setTimeout(() => {
+		if (window.location.hash) {
+			window.scrollTo(0, 0);
+			const target = document.querySelector(window.location.hash);
+			setTimeout(() => {
+				scrollToTarget(target);
+			}, 100);
+		}
+	}, 1);
+
+	// scroll to target on on click
+	let anchorlinks = document.querySelectorAll('a[href^="#"]')
+	for (let item of anchorlinks) {
+		item.addEventListener('click', (e) => {
+			let hashval = item.getAttribute('href');
+			let target = document.querySelector(hashval);
+			scrollToTarget(target);
+			history.pushState(null, null, hashval);
+			e.preventDefault();
+		})
+	}
+
+	function scrollToTarget(target) {
+		const headerOffset = 100;
+		const elementPosition = target.getBoundingClientRect().top;
+		const offsetPosition = elementPosition - headerOffset;
+
+		window.scrollTo({
+			top: offsetPosition,
+			behavior: "smooth",
+		});
+	}
+
+	// swiper in modal
+	const swiperEl = document.querySelector('.swiper-container');
+	if (swiperEl) {
+		const swiper = new Swiper('.swiper-container', {
+			effect: 'coverflow',
+			slidesPerView: 'auto',
+			centeredSlides: true,
+			coverflowEffect: {
+				rotate: 50,
+				stretch: 0,
+				depth: 100,
+				modifier: 1,
+				slideShadows: true,
+			},
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+		});
+	}
+
+	const cards = [].slice.call(document.querySelectorAll(".m-card"));
+	if (cards) fadeIn(cards);
+
+	const waves = document.querySelectorAll('.waves path');
+	window.addEventListener('scroll', throttle(() => {
+		if (waves) elementFromBottom(waves, 'show', 100);
+	}, 50), false);
+
+});
